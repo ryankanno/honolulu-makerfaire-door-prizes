69e60a6b23e79bbb9db74d2c7202a9ff2c847fd8
Ryan Kanno <ryankanno@localkinegrinds.com>
2015-04-07 21:58:59 -1000

    Working.

--- README.md
+++ README.md
@@ -2,19 +2,30 @@
 
 **Note**: This started as a copy and paste of https://github.com/RobSpectre/Twilio-Hackpack-for-Heroku-and-Flask/ (production branch)
 
+# Dependencies
+
+  * Python 2.7.9
+
+# Installation
+
+`pip install -r requirements.txt`
+
+# Running the App
+
+`python app.py`
+
 # User flows
 
 ## User texts raffle number
 
-  - Text xxx-xxx-xxxx your 4-digit raffle ticket
+  - Text xxx-xxx-xxxx your 5-digit raffle ticket
   - System saves your phone number and ticket to a sqlite database
   - System looks up the number against a datastore that has all the winning raffle
     numbers.
   - If number matches winning raffle number:
-    - Play yoda voice indicating the user has won and should report to
-      the front desk.
+    - Text user: winning_copy
   - If number does not match winning raffle number:
-    - Play darth vader voice indicating the user has not won.
+    - Text user: not_winning_copy
 
 ## Admin adds a winning raffle number
 
--- makerfaire/app.py
+++ makerfaire/app.py
@@ -1,32 +1,107 @@
-from database import db_session
-
+from datetime import datetime
 from flask import Flask
 from flask import Response
 from flask import render_template
 from flask import url_for
+from flask import redirect
 from flask import request
 from flask import current_app
-from flask_admin import Admin
-from flask_admin import BaseView
-from flask_admin import expose
-from flask_admin.contrib.sqla import ModelView
-
+from flask_wtf import Form
 from functools import wraps
-
-from models import PhoneNumber
-from models import RaffleNumber
-
 import re
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 from twilio import twiml
-from twilio.util import TwilioCapability
 from twilio.util import RequestValidator
 from urlobject import URLObject
+from wtforms import StringField
+from wtforms_components import DateRange
+from wtforms_components import DateTimeField
+from wtforms.validators import DataRequired
 
-# Declare and configure application
+
+WINNER_COPY = """ WINNER! WINNER! CHICKEN DINNER!"""
+LOSER_COPY = """ NEXT TIME, USE THE FORCE! """
+
+# Application
+
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('local_settings.py')
 
+# Database
+
+engine = create_engine('sqlite:////tmp/makerfaire.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+# Models
+
+class PhoneNumber(Base):
+    __tablename__ = 'PhoneNumber'
+    id = Column(Integer, primary_key=True)
+    phone_number = Column(String(32), unique=True)
+    raffle_numbers = relationship("PhoneNumberRaffleNumber",
+                                  backref="phone_number")
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    def __init__(self, phone_number, created_at, updated_at):
+        self.phone_number = phone_number
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __repr__(self):
+        return '<Phone Number %r>' % (self.phone_number)
+
+
+class PhoneNumberRaffleNumber(Base):
+    __tablename__ = 'PhoneNumberRaffleNumber'
+    id = Column(Integer, primary_key=True)
+    raffle_number = Column(String(8), unique=True)
+    phone_number_id = Column(Integer, ForeignKey('PhoneNumber.id'))
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+
+
+class RaffleWinner(Base):
+    __tablename__ = 'RaffleWinner'
+    id = Column(Integer, primary_key=True)
+    raffle_number = Column(String(8), unique=True)
+    raffle_time = Column(DateTime)
+    item = Column(String(256))
+    is_claimed = Column(Boolean)
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+
+    def __init__(self, raffle_number=None, raffle_time=None, item=None,
+                 is_claimed=None, created_at=None, updated_at=None):
+        self.raffle_number = raffle_number
+        self.raffle_time = raffle_time
+        self.item = item
+        self.is_claimed = is_claimed
+        self.updated_at = updated_at
+        self.created_at = created_at
+
+    def __repr__(self):
+        return '<Raffle Winner %r>' % (self.raffle_number)
+
+
+# Views
 def twilio_secure(func):
     """Wrap a view function to ensure that every request comes from Twilio."""
     @wraps(func)
@@ -39,36 +114,107 @@ def twilio_secure(func):
 
 # raffle_check
 @app.route('/raffle_check', methods=['GET', 'POST'])
-@twilio_secure
+# @twilio_secure
 def check_raffle():
     response = twiml.Response()
+    response_msg = ""
     try:
-        sms_from = request.values.get('From', None)
         sms_body = request.values.get('Body', None)
-    except:
-        pass
+        if sms_body:
+            searches = re.search(r'^[0-9]{4,5}$', sms_body.strip())
+            if searches:
+                raffle_number = searches.group()
+                sms_from = request.values.get('From', None)
+
+                if sms_from:
+                    # retrieve phone number
+                    number = PhoneNumber.query.filter(PhoneNumber.phone_number == sms_from.strip()).first()
+
+                    # if number found
+                    if number:
+                        found_number = [num for num in number.raffle_numbers if num.raffle_number == raffle_number]
+                        if found_number:
+                            response_msg = "You've already submitted this number."
+                            response.sms(response_msg)
+                            return str(response)
+                        else:
+                            phone_number_raffle_number = PhoneNumberRaffleNumber(raffle_number=raffle_number, phone_number_id=number.id, updated_at=datetime.utcnow(), created_at=datetime.utcnow())
+                            number.raffle_numbers.append(phone_number_raffle_number)
+                            db_session.add(number)
+                            db_session.commit()
+                    else:
+                        # save from and number
+                        phone_number = PhoneNumber(sms_from.strip(),
+                                datetime.utcnow(), datetime.utcnow())
+                        phone_number_raffle_number = PhoneNumberRaffleNumber(raffle_number=raffle_number,
+                                updated_at=datetime.utcnow(),
+                                created_at=datetime.utcnow())
+                        phone_number.raffle_numbers.append(phone_number_raffle_number)
+                        db_session.add(phone_number)
+                        db_session.commit()
+
+                    winner = RaffleWinner.query.filter(RaffleWinner.raffle_number == raffle_number).first()
+                    response_msg = WINNER_COPY if winner else LOSER_COPY
+                else:
+                    return
+            else:
+                response_msg = "Please submit a valid raffle number!"
+        else:
+            response_msg = "Please submit a valid raffle number!"
+    except Exception as e:
+        print e
+    response.sms(response_msg)
     return str(response)
+
+
+# add_raffle_winner
+@app.route('/raffle_winners/add', methods=['GET', 'POST'])
+def add_raffle_winner():
+    form = RaffleWinnerForm()
+
+    if form.validate_on_submit():
+        raffle_winner = RaffleWinner()
+        form.populate_obj(raffle_winner)
+        raffle_winner.is_claimed = False
+        raffle_winner.updated_at = datetime.utcnow()
+        raffle_winner.created_at = datetime.utcnow()
+        db_session.add(raffle_winner)
+        db_session.commit()
+        return redirect('/')
+
+    return render_template('add_raffle_winner.html', form=form)
 
 
 # default view
 @app.route('/', methods=['GET'])
 def show_raffle_numbers():
-    return render_template('slash.html')
+    unclaimed_raffle_winners = RaffleWinner.\
+        query.filter(RaffleWinner.is_claimed == False)
+    return render_template('slash.html',
+                           unclaimed_raffle_winners=unclaimed_raffle_winners)
 
 
-class RaffleView(BaseView):
+# shutdown database
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
-    @expose('/')
-    def index(self):
-        return self.render('admin_index.html')
 
-    @expose('/raffle', methods=['GET'])
-    def add_raffle_view():
-        return render_template('admin_add_raffle.html')
+def init_db():
+    Base.metadata.create_all(engine)
 
-    @expose('/raffle', methods=['POST'])
-    def add_raffle_action():
-        pass
+
+# forms
+class RaffleWinnerForm(Form):
+    raffle_time = DateTimeField(
+        'raffle_time',
+        validators=[DateRange(
+            min=datetime(2015, 5, 14),
+            max=datetime(2015, 5, 16)
+        )]
+    )
+    raffle_number = StringField('raffle_number', validators=[DataRequired()])
+    item = StringField('item', validators=[DataRequired()])
 
 
 def validate_twilio_request():
@@ -87,7 +233,3 @@ def validate_twilio_request():
     else:
         return False
     return validator.validate(url, request.form, signature.encode('UTF-8'))
-
-
-admin = Admin(app)
-admin.add_view(RaffleView(name="Raffle"))
